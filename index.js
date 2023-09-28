1574280c7877b69394751442ea44ed812fde9b97
balafreelancer87 <balafreelancer87@gmail.com>
2023-09-29 02:30:46 +0530

    db connection

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Database Connection is Succesfull"))
+    .catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", async (req, res) => {
+    res.status(200).json("Welcome")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+});
+
