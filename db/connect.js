6ab8721bac2d5ce9478a44ed6fd6595b949982e1
balafreelancer87 <balafreelancer87@gmail.com>
2023-09-29 07:00:42 +0530

    db connection moved separate

---
+++ db/connect.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+
+//strictQuery false by default. That means that all the fields will be saved in the database, even if some of them are not specified in the schema model
+mongoose.set("strictQuery", false);
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+mongoose
+    .connect(process.env.MONGO_URL, options)
+    .then((res) => {
+        console.log("Database Connected Successfully");
+        console.log(`Database Host: ${res.connection.host}`);
+    })
+    .catch((err) => {
+        console.error(`Database Error: ${err}`);
+    });
+
+}
+
+module.exports = connectDatabase;
+
--- index.js
+++ index.js
@@ -1,34 +1,71 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
 
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
+const logger = require("morgan");
 
+// db connection
+const connectDatabase = require("./db/connect");
+connectDatabase();
+
+// all routes
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 
 
-const PORT = process.env.PORT || 5000;
+//logger
+app.use(logger("dev"));
 
-mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("Database Connection is Succesfull"))
-    .catch((err) => console.log(err));
-
+//cors
 app.use(cors());
+
+// parse application/json, basically parse incoming Request Object as a JSON Object
 app.use(express.json());
 
+// parse incoming Request Object if object, with nested objects, or generally any type.
+app.use(express.urlencoded({ extended: true }));
 
+
+// index page
 app.get("/", async (req, res) => {
-    res.status(200).json("Welcome")
+    res.status(200).json("Welcome to API")
 });
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT}`);
+//catch 404 and user enters invalid url
+app.use((req, res, next) => {
+    res.status(404).json({
+        error:'Url not found'
+    })
+})
+
+module.exports = app;
+
+//Start server and console log the used port
+const server = app.listen(PORT, function () {
+  console.log(
+    `Server is up and running on  http://localhost:${server.address().port}`
+  );
 });
+
+process.on('unhandledRejection', (err, p)=>{
+    console.log(`Error: ${err.message}`);
+    console.log("Possibly Unhandled Rejection at: Promise ", p);
+    console.log('Shutting down the server due to unhandled rejection error');
+    server.close(()=>{
+        process.exit(1);
+    });
+})
+
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log('Shutting down the server due to uncaught exception error');
+    server.close(()=>{
+        process.exit(1);
+    });
+})
 
