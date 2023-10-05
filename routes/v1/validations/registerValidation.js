e4f40bb017081a71f2e5ffd4f3823b43218b7384
balafreelancer87 <balafreelancer87@gmail.com>
2023-10-05 16:18:31 +0530

    compare password validation added

--- routes/v1/validations/registerValidation.js
+++ routes/v1/validations/registerValidation.js
@@ -1,27 +1,42 @@
-const { check, validationResult } = require('express-validator');
+const { check } = require('express-validator');
 const {inputValidationMiddleware} = require("../../../middlewares/customValidations");
 
 const registerValidation = [
-    check('username')
-      .trim()
-      .isLength({ min: 3 })
-      .escape()
-      .withMessage('A valid username with minimum 3 letter is required'),
-    check('email')
-      .trim()
-      .isEmail()
-      .normalizeEmail()
-      .withMessage('A valid email address is required'),
-    check('password')
-      .trim()
-      .isLength({ min: 6 })
-      .escape()
-      .withMessage('A valid password with minimum 6 letter is required'),
-    // check('message')
-    //   .trim()
-    //   .isLength({ min: 5 })
-    //   .escape()
-    //   .withMessage('A message is required'),
+
+  check("username")
+    .not()
+    .isEmpty()
+    .withMessage("username cannot be empty")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('A valid username with minimum 3 letter is required'),
+
+  check("password")
+    .not()
+    .isEmpty()
+    .withMessage("password cannot be empty")
+    .trim()
+    .isLength({ min: 6 })
+    .escape()
+    .withMessage('A valid password with minimum 6 letter is required'),
+
+  check("email")
+    .not()
+    .isEmpty()
+    .withMessage("email cannot be empty")
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage('A valid email address is required'),
+
+  check("confirmpassword")
+    .not()
+    .isEmpty()
+    .withMessage("confirm password cannot be empty")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("compare password must match with password field"),
+
     inputValidationMiddleware
   ];
 
